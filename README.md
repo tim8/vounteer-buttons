b4cb09e1381f31254cc679a211ee8515b3346ee8
tim <timnakamura@gmail.com>
2015-08-18 16:32:58 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# glowing-bear
 # vounteer-buttons
---
+++ plugin.rb
@@ -0,0 +1,16 @@
+# name: volunteer buttons
+# about: click to volunteer
+# version: 1.0
+# author: mrl
+# Supported Discourse version: v1.3
+
+register_asset 'javascripts/volunteer.js.erb', :server_side
+register_asset 'javascripts/volunteer_buttons.js'
+
+register_css <<CSS
+
+span.chcklst-stroked {
+	text-decoration: line-through;
+}
+
+CSS
