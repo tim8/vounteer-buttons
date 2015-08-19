fae2a0b6e4cac03205fc5c34109aae661b1184a1
tim <timnakamura@gmail.com>
2015-08-19 01:21:05 -0600

    bbcode

--- assets/javascripts/volunteer_buttons.js
+++ assets/javascripts/volunteer_buttons.js
@@ -1,16 +1,19 @@
 Discourse.PostView.reopen({
   createVolunteerButton: function($post) {
     
-   
-    if (!Discourse.User.currentProp('username') && this.get('post.topic.category').id != 1 ) { return };
-    
     var age = new Date() - new Date(this.get('post.topic.created_at')),
         buttons = $post.find(".volunteer-button"),
         user = Discourse.User.currentProp('username');
-  
-    if(age >= 86400000){
-        $(buttons).remove();
+    
+    if(!buttons){return};
+    if(!user){
+        $(buttons).after('Log In to volunteer');
     }
+    if (!user || this.get('post.topic.category').id != 1 || age >= 86400000) { 
+        $(buttons).remove();
+        return
+        };
+    
       $(buttons).click(function(obj) {
         var elem = $(obj.currentTarget),
             show = this.getAttribute("show"),
